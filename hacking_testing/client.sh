002a16827664e5892007fddcd4c85f22e82ab288
neverix <nev@neverix.io>
2022-11-23 09:35:42 +0300

    Replace breaking change

--- hacking_testing/client.sh
+++ hacking_testing/client.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-exec bin/minetest --name me --password whyisthisnecessary --address 0.0.0.0 --port 30000 --go --record --record-port "tcp://*:9001"  # --dumb --dumb-port "tcp://*:9000"
+exec bin/minetest --name me --password whyisthisnecessary --address 0.0.0.0 --port 30000 --go --dumb --dumb-port "tcp://*:9000"  # --record --record-port "tcp://*:9001"
