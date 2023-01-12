ad86c4c8fdfd22f998a9146dfb6b51d28941e83d
Josiah VanderZee <josiah_vanderzee@mediacombb.net>
2023-01-12 16:58:27 -0600

    Define BUILD_HEADLESS symbol in cmake_config.h.in
    The symbol will be defined as 1 as long as the BUILD_HEADLESS CMake var
    is a truthy value.

--- src/client/render/pipeline.cpp
+++ src/client/render/pipeline.cpp
@@ -17,6 +17,7 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
 
+#include "cmake_config.h"
 #include "pipeline.h"
 #include "client/client.h"
 #include "client/hud.h"
