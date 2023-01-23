e76385130a90c19b5902853347fa81ec515cb1c5
AI_WAIFU <aiwaifu42@gmail.com>
2023-01-23 14:55:38 +0000

    fix typos

--- cmake/Modules/FindZmqpp.cmake
+++ cmake/Modules/FindZmqpp.cmake
@@ -2,32 +2,37 @@ option(ENABLE_SYSTEM_ZMQPP "Use ZMQPP from system" TRUE)
 mark_as_advanced(ZMQPP_LIBRARY ZMQPP_INCLUDE_DIR)
 set(USE_SYSTEM_ZMQPP FALSE)
 
+message(${CMAKE_SOURCE_DIR})
+
 
 if(ENABLE_SYSTEM_ZMQPP)
 	find_library(ZMQPP_LIBRARY NAMES zmqpp)
 	find_path(ZMQPP_INCLUDE_DIR NAMES zmqpp.hpp PATH_SUFFIXES zmqpp)
 	
 	if(ZMQPP_LIBRARY AND ZMQPP_INCLUDE_DIR)
-		message (STATUS "Using GMP provided by system.")
+		message (STATUS "Using ZMQPP provided by system.")
 		set(USE_SYSTEM_GMP TRUE)
 	else()
-		message (STATUS "Detecting GMP from system failed.")
+		message (STATUS "Detecting ZMQPP from system failed.")
 	endif()
 
 endif()
 
-if(NOT USE_SYSTEM_GMP)
-	message(STATUS "Using zmqpp submodule")
-	find_path(ZMQPP_INCLUDE_DIR NAMES zmqpp.hpp 
+if(NOT USE_SYSTEM_ZMQPP)
+	message(STATUS "Using ZMQPP submodule")
+	find_path(ZMQPP_INCLUDE_DIR NAMES zmqpp/zmqpp.hpp 
 		PATHS
-		${CMAKE_SOURCE_DIR}/lib/zmqpp/src/zmqpp)
+		${CMAKE_SOURCE_DIR}/lib/zmqpp/src/
+		PATH_SUFFIXES zmqpp)
 
 	find_library(ZMQPP_LIBRARY NAMES zmqpp 
 		PATHS ${CMAKE_SOURCE_DIR}/lib/zmqpp/build/max-g++)
+
+	message(${ZMQPP_INCLUDE_DIR})
+	message(${ZMQPP_LIBRARY})
 	
 	if(ZMQPP_LIBRARY AND ZMQPP_INCLUDE_DIR)
 		message (STATUS "Using ZMQPP provided by the submodule.")
-		set(USE_SYSTEM_GMP TRUE)
 	else()
 		message (STATUS "ZMQPP submodule detection failed, please navigate to lib/zmqpp, initialize the submodule with \"git submodule update --init\", and then use \"make\" to build zmqpp")
 	endif()
