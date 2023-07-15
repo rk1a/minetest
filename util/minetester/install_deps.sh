35f9b5b50def309fc28b8f4dc1242d9c20de10e7
AI_WAIFU <aiwaifu@protonmail.com>
2023-07-15 23:17:31 +0100

    add patchelf dependency

--- util/minetester/install_deps.sh
+++ util/minetester/install_deps.sh
@@ -7,7 +7,7 @@ OS=$(lsb_release -si)
 if [ "$OS" == "Ubuntu" ]; then
     # If it's Ubuntu, execute the command
     echo "You are running Ubuntu. Executing command..."
-    sudo apt-get install xvfb g++ make libzmq3-dev libtool pkg-config build-essential autoconf automake libc6-dev cmake libpng-dev libjpeg-dev libxi-dev libgl1-mesa-dev libsqlite3-dev libogg-dev libvorbis-dev libopenal-dev libcurl4-gnutls-dev libfreetype6-dev zlib1g-dev libgmp-dev libjsoncpp-dev libzstd-dev libluajit-5.1-dev protobuf-compiler
+    sudo apt-get install xvfb g++ make libzmq3-dev libtool pkg-config build-essential autoconf automake libc6-dev cmake libpng-dev libjpeg-dev libxi-dev libgl1-mesa-dev libsqlite3-dev libogg-dev libvorbis-dev libopenal-dev libcurl4-gnutls-dev libfreetype6-dev zlib1g-dev libgmp-dev libjsoncpp-dev libzstd-dev libluajit-5.1-dev protobuf-compiler patchelf
 else
     # If it's not Ubuntu, print a message and exit
     echo "This script only works on Ubuntu. Exiting."
