9f8cbd7f1ebb9326b0c7ddf26d8b719ee2cca440
Curtis <chuebner@pop-os.localdomain>
2023-11-28 14:09:41 +0000

    gitignore and pop os support

--- util/minetester/install_deps.sh
+++ util/minetester/install_deps.sh
@@ -4,9 +4,9 @@
 OS=$(lsb_release -si)
 
 # Check if the OS is Ubuntu
-if [ "$OS" == "Ubuntu" ]; then
+if [ "$OS" == "Ubuntu" ] || [ "$OS" == "Pop" ]; then
     # If it's Ubuntu, execute the command
-    echo "You are running Ubuntu. Executing command..."
+    echo "You are running Ubuntu or Pop. Executing command..."
     sudo apt-get install xvfb g++ make libzmq3-dev libtool pkg-config build-essential autoconf automake libc6-dev cmake libpng-dev libjpeg-dev libxi-dev libgl1-mesa-dev libsqlite3-dev libogg-dev libvorbis-dev libopenal-dev libcurl4-gnutls-dev libfreetype6-dev zlib1g-dev libgmp-dev libjsoncpp-dev libzstd-dev libluajit-5.1-dev protobuf-compiler patchelf
 elif [ "$OS" == "Arch" ]; then
     echo "You are running Arch. Executing command..."
